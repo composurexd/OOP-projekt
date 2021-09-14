e5cf8f09ae492bd98d0783bacfdaec8945c5a9ea
Petter Blomkvist <voxedg@gmail.com>
2021-09-14 12:59:27 +0200

    Setup testing

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,6 @@ module org.runtimeterror {
     requires transitive javafx.graphics;
     requires transitive javafx.controls;
     requires javafx.fxml;
+    opens org.runtimeterror;
     exports org.runtimeterror;
 }
